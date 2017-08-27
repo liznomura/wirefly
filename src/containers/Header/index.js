5e2abf19c84633bf1decfa109002aa2e22c17585
Liz Nomura <lizn@hawaii.edu>
2017-08-27 00:49:17 -1000

    added header icons, not functional yet

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import Logo from '../../wirefly_logo_white.png';
+import Save from '../../svg/save.svg';
+import Login from '../../svg/profile.svg';
+import ExportCode from '../../svg/code.svg';
+import Share from '../../svg/share.svg';
 
 class Header extends Component {
   render() {
     return (
-      <div className="header"/>
+      <div className="header">
+        <img src={ Logo } className="logo" alt="logo"/>
+        <img src={ Share } className="headerIcon" alt="save"/>
+        <img src={ ExportCode } className="headerIcon" alt="save"/>
+        <img src={ Save } className="headerIcon" alt="save"/>
+        <img src={ Login } className="headerIcon" alt="login"/>
+      </div>
       );
   }
 }
