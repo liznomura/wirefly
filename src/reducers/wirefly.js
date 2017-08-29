319a77906988919ffe742074f2ffcbd8c3fc6fcf
Benjamin Kwok <benkwokdev@gmail.com>
2017-08-28 14:31:02 -1000

    fixed reducer and PageTree bugs

--- src/reducers/wirefly.js
+++ src/reducers/wirefly.js
@@ -1,7 +1,5 @@
 import { SET_TOOL, ADD_ELEMENT, TOGGLE_VISIBILITY } from '../actions';
 import _ from 'lodash';
-
-
 import PageTree from './PageTree';
 import arrayTree from './PageTree/arrayTree.json';
 import createTree from './PageTree/createTree';
@@ -14,37 +12,12 @@ const initialState = {
   PageTree: Page
 };
 
-// const initialState = {
-//   tool: '',
-//   isToolbarVisible: false,
-//   containers: [
-//     {
-//       id: 0,
-//       type: 'page',
-//       width: '100%',
-//       height: '100%',
-//       properties: {},
-//       children: [
-//         {
-//           id: 29,
-//           type: 'container',
-//           width: '100%',
-//           height: '100%',
-//           properties: {},
-//           children: []
-//         }
-//       ]
-//     }
-//   ]
-// };
-
 const wireflyReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_ELEMENT:
       let newTree = _.cloneDeep(state.PageTree);
-      console.log('HERE', action.element.parentId);
       newTree.add(action.element.properties, action.element.parentId, newTree.traversalBF);
-      return newTree;
+      return Object.assign({}, state, { PageTree: newTree });
 
     case SET_TOOL:
       return Object.assign({}, state, { tool: action.tool });
