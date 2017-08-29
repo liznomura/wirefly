69d618111d43391e8783d2670d097d39748d1979
Liz Nomura <lizn@hawaii.edu>
2017-08-28 14:32:16 -1000

    removed extra actions from undoable filter

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 import wireflyReducer from './wirefly';
 
-import { ADD_ELEMENT, SET_TOOL, TOGGLE_VISIBILITY, UNDO_ACTION, REDO_ACTION } from '../actions';
+import { ADD_ELEMENT, UNDO_ACTION, REDO_ACTION } from '../actions';
 import undoable, { includeAction } from 'redux-undo';
 
 const rootReducer = combineReducers({
   wirefly: undoable(wireflyReducer, {
-    filter: includeAction([SET_TOOL, TOGGLE_VISIBILITY, ADD_ELEMENT]),
+    filter: includeAction([ADD_ELEMENT]),
     limit: 10,
     debug: true,
     undoType: UNDO_ACTION,
